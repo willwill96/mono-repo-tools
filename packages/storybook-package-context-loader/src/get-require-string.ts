b2b038293ef2fdf29ac3973a87973361d280e97e
William Will <10997562+willwill96@users.noreply.github.com>
2022-09-02 23:20:49 +0000

    fix: ordering of readme and changelog

--- packages/storybook-package-context-loader/src/get-require-string.ts
+++ packages/storybook-package-context-loader/src/get-require-string.ts
@@ -3,6 +3,8 @@ import fs from "fs";
 import { execSync } from "child_process";
 import type { LoaderOptions } from "./types";
 
+const UNDEFINED_STRING = "undefined";
+
 function getRootPackageDirectory(
   topLevelDirectory: string,
   fileLocation: string
@@ -30,14 +32,20 @@ export function getRequireString(fileLocation: string, options: LoaderOptions) {
   const pkgJsonPath = path.join(rootPackageDirectory, "package.json");
   if (fs.existsSync(pkgJsonPath) && options.enablePkgJson !== false) {
     requireStrings.push(`require('${pkgJsonPath}')`);
-  }
-  const changelogPath = path.join(rootPackageDirectory, "CHANGELOG.md");
-  if (fs.existsSync(changelogPath) && options.enableChangelog !== false) {
-    requireStrings.push(`require('${changelogPath}')`);
+  } else {
+    requireStrings.push(UNDEFINED_STRING);
   }
   const readmePath = path.join(rootPackageDirectory, "README.md");
   if (fs.existsSync(readmePath) && options.enableReadme !== false) {
     requireStrings.push(`require('${readmePath}')`);
+  } else {
+    requireStrings.push(UNDEFINED_STRING);
+  }
+  const changelogPath = path.join(rootPackageDirectory, "CHANGELOG.md");
+  if (fs.existsSync(changelogPath) && options.enableChangelog !== false) {
+    requireStrings.push(`require('${changelogPath}')`);
+  } else {
+    requireStrings.push(UNDEFINED_STRING);
   }
 
   return requireStrings.join(",");
