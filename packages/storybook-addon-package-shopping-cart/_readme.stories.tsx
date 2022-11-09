2c70e1ffe5a4a47acf65d350e1a3a4d16a2fb630
William Will <10997562+willwill96@users.noreply.github.com>
2022-11-09 05:11:23 +0000

    update stories

--- packages/storybook-addon-package-shopping-cart/_readme.stories.tsx
+++ packages/storybook-addon-package-shopping-cart/_readme.stories.tsx
@@ -3,12 +3,13 @@ import { Description } from "@storybook/components";
 import { ThemeProvider, ensure, themes } from "@storybook/theming";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const markdown = require("./README.md");
-export const StorybookPackageContextLoader = () => (
+export const StorybookAddonPackageShoppingCart = () => (
   <ThemeProvider theme={ensure(themes.light)}>
     <Description markdown={markdown} />
   </ThemeProvider>
 );
 
 export default {
-  title: "Published Packages / Storybook / Storybook Package Context Loader",
+  title:
+    "Published Packages / Storybook / Storybook Addon Package Shopping Cart",
 };
